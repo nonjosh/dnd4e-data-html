a3ad53dbcd6d56f58446f05b9125d28ddaecb3e6
nonjosh <nonjosh@gmail.com>
2020-06-05 23:59:51 +0800

    first commit

---
+++ compendium/backgrounds/rename.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for f in `find . -type f | grep \.html`
+   do
+   title=$( awk 'BEGIN{IGNORECASE=1;FS="<title>|</title>";RS=EOF} {print $2}' "$f" )
+   title=$( echo "$title" | tr '\/\\' '_' )
+   mv -fi "$f" "${title//[ ]/-}".html
+done
